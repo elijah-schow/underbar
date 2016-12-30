0522a4db350d9f7080d556902bd0c981b141150c
Elijah Schow <elijah.schow@gmail.com>
2016-12-30 16:24:34 -0600

    Implement _.reject()

--- src/underbar.js
+++ src/underbar.js
@@ -99,6 +99,11 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, not);
+
+    function not(value){
+      return !test(value);
+    }
   };
 
   // Produce a duplicate-free version of the array.
